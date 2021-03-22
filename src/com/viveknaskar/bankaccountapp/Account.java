4243484ec69c84de348807c2b9a8cda5638895ef
Vivek Naskar <viveknaskar2912@gmail.com>
2021-03-23 01:28:58 +0530

    Generated Account Number

--- src/com/viveknaskar/bankaccountapp/Account.java
+++ src/com/viveknaskar/bankaccountapp/Account.java
@@ -16,6 +16,8 @@ public abstract class Account implements IBaseRate {
     String accountNumber;
     double rate;
 
+    static int index = 10000;
+
     /**
      * Constructor to initialise base properties and initialise the account
      */
@@ -25,6 +27,29 @@ public abstract class Account implements IBaseRate {
         this.socialSecurityNumber = socialSecurityNumber;
         balance = initDeposit;
         System.out.println("Name: " + name + "\nSSN: " + socialSecurityNumber + "\nbalance: " + balance);
+
+        /**
+         * Set account number
+         */
+        index++;
+        this.accountNumber = setAccountNumber();
+        System.out.println("Account Number: " + this.accountNumber);
+    }
+
+    /**
+     * Account Number should have the below requirements:
+     * last two digits of SSN
+     * unique 5-digit number
+     * random 3-digit number
+     * @return Account Number
+     */
+    private String setAccountNumber() {
+        String lastTwoOfSSN = socialSecurityNumber
+                .substring(socialSecurityNumber.length()-2, socialSecurityNumber.length());
+        int uniqueId = index;
+        int randomNumber = (int) (Math.random() * Math.pow(10, 3));
+        return lastTwoOfSSN + uniqueId + randomNumber;
+
     }
 
     /**
