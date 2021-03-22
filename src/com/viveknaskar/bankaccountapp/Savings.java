3b2644e255269893eff4c5ed4669d8f0a7fb596e
Vivek Naskar <viveknaskar2912@gmail.com>
2021-03-23 02:10:41 +0530

    Added savings account features

--- src/com/viveknaskar/bankaccountapp/Savings.java
+++ src/com/viveknaskar/bankaccountapp/Savings.java
@@ -1,5 +1,9 @@
 package com.viveknaskar.bankaccountapp;
 
+/**
+ * Savings Account holders are given safety deposit box identified by 3-digit number
+ * accessed with 4-digit PIN
+ */
 public class Savings extends Account {
 
     /**
@@ -14,13 +18,24 @@ public class Savings extends Account {
     public Savings(String name, String socialSecurityNumber, double initDeposit) {
         super(name, socialSecurityNumber, initDeposit);
         accountNumber = "1" + accountNumber;
+        setSafetyDepositBox();
+    }
+
+    private void setSafetyDepositBox() {
+        safetyDepositBoxId = (int) (Math.random() * Math.pow(10, 3));
+        safetyDepositBoxKey = (int) (Math.random() * Math.pow(10, 4));
     }
 
     /**
      * List any methods specific to the Savings account
      */
     public void showInfo() {
-        super.showInfo();
         System.out.println("Account type: Savings");
+        super.showInfo();
+        System.out.println(
+                " Your Savings Account features: " +
+                "\n Safety Deposit Box Id: " + safetyDepositBoxId +
+                "\n Safety Deposit Box Key: " + safetyDepositBoxKey
+        );
     }
 }
