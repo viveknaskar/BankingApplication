f1ee1b3520a83cc9014ed7f0093383dbf1b40ed7
Vivek Naskar <viveknaskar2912@gmail.com>
2021-03-25 03:18:45 +0530

    Hovering over the list of account holders read from the CSV file

--- src/com/viveknaskar/bankaccountapp/BankAccountApp.java
+++ src/com/viveknaskar/bankaccountapp/BankAccountApp.java
@@ -24,10 +24,22 @@ public class BankAccountApp {
         List<String[]> newAccountHolders = com.viveknaskar.bankaccountapp.helper.CSVHelper.
                 read("E:\\Workspace\\BankAccountApplication\\mock_account_holders.csv");
         for (String[] accountHolder: newAccountHolders) {
-            System.out.println("New account holder is: ");
-            for (String s : accountHolder) {
-                System.out.println(s);
-            }
+
+            System.out.println("New account holder is:- ");
+            String name = accountHolder[0];
+            String sSN = accountHolder[1];
+            String accountType = accountHolder[2];
+            double initDeposit = Double.parseDouble(accountHolder[3]);
+
+            System.out.println("Name: " + name
+                    + "\nSSN: " + sSN
+                    + "\nAccount: " + accountType
+                    + "\nInitial Deposit: $" + initDeposit
+                    + "\n"
+            );
+
+
+
         }
 
     }
